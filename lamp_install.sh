3fc7cf18f5f15a0f2d2034adcd0614ad0f053751
AdasTamosiunas565 <167907944+AdasTamosiunas565@users.noreply.github.com>
2024-04-23 21:34:39 +0300

    Update lamp_install.sh to lower versions

--- lamp_install.sh
+++ lamp_install.sh
@@ -18,26 +18,26 @@ install_component() {
     if [ ! -d "$installation_dir/$dir_name" ]; then
         # Download and extract
         wget -P /tmp "$url"
-        tar -xzvf /tmp/$(basename "$url") -C /tmp
+        tar -xzvf "/tmp/$(basename "$url")" -C /tmp
 
         # Compile and install
-        cd /tmp/$(basename "$url" .tar.gz)
+        cd "/tmp/$(basename "$url" .tar.gz)"
         ./configure --prefix="$installation_dir/$dir_name"
         make && sudo make install
     fi
 }
 
-# Install Apache
-install_component "2.4.52" "https://archive.apache.org/dist/httpd/httpd-2.4.52.tar.gz" "apache"
+# Install Apache 2.2.34 (older version)
+install_component "2.2.34" "https://archive.apache.org/dist/httpd/httpd-2.2.34.tar.gz" "apache"
 
-# Install MariaDB
-install_component "10.7.1" "https://downloads.mariadb.org/interstitial/mariadb-10.7.1/source/mariadb-10.7.1.tar.gz" "mariadb"
+# Install MariaDB 10.3.31 (older version)
+install_component "10.3.31" "https://downloads.mariadb.org/interstitial/mariadb-10.3.31/source/mariadb-10.3.31.tar.gz" "mariadb"
 
-# Install PHP
-install_component "8.1.3" "https://www.php.net/distributions/php-8.1.3.tar.gz" "php"
+# Install PHP 7.4.27 (older version)
+install_component "7.4.27" "https://www.php.net/distributions/php-7.4.27.tar.gz" "php"
 
-# Start Apache and MariaDB
+# Start Apache
 sudo "$installation_dir/apache/bin/apachectl" start
-sudo "$installation_dir/mariadb/bin/mysqld_safe" --datadir="$installation_dir/mariadb/data" &
 
-echo "Installation completed."
+# Start MariaDB
+sudo "$installation_dir/mariadb/bin/mysqld" --basedir="$installation_dir/mariadb" --datadir="$installation_dir/mariadb/data" --pid-file="$installation_dir/mariadb/mysqld.pid" --socket="$installation_dir/mariadb/mysql.sock" --port=3306 --user=mysql &
